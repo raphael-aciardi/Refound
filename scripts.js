e0116502fd33d59479b3140be37df8101fe0fc80
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-10-14 14:10:14 -0300

    Creating an objetc with the context of expensive

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
-
-
+const expense = document.getElementById("expense")
+const category = document.getElementById("category")
 
 amount.oninput = () => {
     value = amount.value.replace(/\D/g, "")
@@ -10,8 +11,6 @@ amount.oninput = () => {
     amount.value = formatCurrencyBRL(value)
 }
 
-
-
 function formatCurrencyBRL(value){
     value = value.toLocaleString("pt-BR", {
     style: "currency",
@@ -19,4 +18,19 @@ function formatCurrencyBRL(value){
     })
     
     return value
+}
+
+form.onsubmit = (event)=> {
+    event.preventDefault()
+
+    const newExpense = {
+        id: new Date().getTime(),
+        expense: expense.value,
+        category_id: category.value,
+        category_name: category.options[category.selectedIndex].text,
+        amount: amount.value,
+        create_at: new Date()
+    }
+
+    console.log(newExpense)
 }

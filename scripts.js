5112e78f872e6c00f44ecd868d8befcd09ec6daf
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2024-10-14 13:46:46 -0300

    Creating a function to format de value of the money

--- scripts.js
+++ scripts.js
@@ -1,6 +1,22 @@
 const amount = document.getElementById("amount")
 
+
+
 amount.oninput = () => {
     value = amount.value.replace(/\D/g, "")
-    amount.value = value
+
+    value = Number(value) / 100
+
+    amount.value = formatCurrencyBRL(value)
+}
+
+
+
+function formatCurrencyBRL(value){
+    value = value.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL"
+    })
+    
+    return value
 }
